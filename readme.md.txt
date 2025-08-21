c1e8c3e85de0a249ac640418dfd334c3228126fa
pedromalta98 <pedromalta2012@live.com>
2025-08-20 23:01:59 -0300

    Primeiro commit: Calculadora de partidas rankeadas adicionada

---
+++ readme.md.txt
@@ -0,0 +1,36 @@
+Desenvolvi uma calculadora de partidas rankeadas para um desafio de código proposto pela Formação em Lógica de Programação da DIO.
+
+Nesse desafio eu precisava seguir os seguintes pontos:
+
+ # 2️⃣ Calculadora de partidas Rankeadas
+**O Que deve ser utilizado**
+
+- Variáveis
+- Operadores
+- Laços de repetição
+- Estruturas de decisões
+- Funções
+
+## Objetivo:
+
+Criar uma função que recebe como parâmetro a quantidade de vitórias e derrotas de um jogador,
+depois disso retornar o resultado para uma variável, o saldo de Rankeadas deve ser feito através do calculo (vitórias - derrotas)
+
+Se vitórias for menor do que 10 = Ferro
+Se vitórias for entre 11 e 20 = Bronze
+Se vitórias for entre 21 e 50 = Prata
+Se vitórias for entre 51 e 80 = Ouro
+Se vitórias for entre 81 e 90 = Diamante
+Se vitórias for entre 91 e 100= Lendário
+Se vitórias for maior ou igual a 101 = Imortal
+
+## Saída
+
+Ao final deve se exibir uma mensagem:
+"O Herói tem de saldo de **{saldoVitorias}** está no nível de **{nivel}**"
+
+
+-- Não apenas segui o desafio proposto como adicionei também uma patente para o jogador, o inventário do mesmo e duas saídas mais detalhadas.
+
+*Esse pequeno projeto foi feito por mim, que estou estudando lógica de programação há pouco tempo. Claro que terão erros e talvez falta do uso de alguma boa prática, mas sigo em desenvolvimento e a cada projeto busco melhorar ainda mais. Toda ajuda, comentários e críticas são muito bem vindos! 
+Para isso sinta-se a vontade para ajudar no meu desenvolvimento como um futuro profissional da área de tecnologia através do meu LinkedIn ( João Pedro Malta )
---
+++ src/calculadora.js
@@ -0,0 +1,89 @@
+// Calculadora de partidas ranqueadas
+// Objetivo: Criar uma função que receba como parâmetro a quantidade de vitórias e derrotas de um jogador,
+// depois disso retornar o resultado para uma variável, o saldo de Rankeadas deve ser feito através do calculo (vitórias - derrotas)
+
+// O que deve ser utilizado: variáveis, operadores, laços de repetição, estrutura de decisões e funções.
+// 10 vitórias = Ferro; entre 11 e 20 = Bronze; entre 21 e 50 = Prata; entre 51 e 80 = Ouro; entre 81 e 90 = Diamante; 
+// entre 91 e 100 = Lendário; maior ou igual a 101 = imortal
+
+// ## Saída: Ao final deve se exibir uma mensagem: "O Herói tem de saldo de **{saldoVitorias}** está no nível de **{nivel}**"
+
+const jogador = "Noob Player";    // Utilização de constante para o nome do jogador
+let nivel = 65;                   // Utilização de variáveis
+let partidas = 48;
+let vitorias = 40;
+let derrotas = 8;
+
+function saldoDeRankeadas(vitorias, derrotas) {                // Utilização de função para calcular o saldo de partidas ranqueadas
+    return vitorias - derrotas;
+}
+
+let saldoVitorias = saldoDeRankeadas(vitorias, derrotas);
+
+let ranking;
+
+if (saldoVitorias < 10) {                  // Utilização de estrutura de decisão para determinar o ranking
+    ranking = "Ferro";
+} else if (saldoVitorias >= 11 && saldoVitorias <= 20) {
+    ranking = "Bronze";
+} else if (saldoVitorias >= 21 && saldoVitorias <= 50) {
+    ranking = "Prata";
+} else if (saldoVitorias >= 51 && saldoVitorias <= 80) {
+    ranking = "Ouro";
+} else if (saldoVitorias >= 81 && saldoVitorias <= 90) {
+    ranking = "Diamante";
+} else if (saldoVitorias >= 91 && saldoVitorias <= 100) {
+    ranking = "Lendário";
+} else {
+    ranking = "Imortal";    
+}
+
+let patente;  // Variável para armazenar a patente do jogador
+switch (nivel) {              // Utilização de switch para determinar a patente do jogador
+    case 10:
+        patente = "Soldado";
+        break;
+    case 15:
+        patente = "Cabo"
+        break;
+    case 25:
+        patente = "Sargento";
+        break;
+    case 35:
+        patente = "Tenente";
+        break;
+    case 45:
+        patente = "Capitão";
+        break;
+    case 55:
+        patente = "Major";
+        break;
+    case 65:
+        patente = "Coronel";
+        break;
+    case 75:
+        patente = "General";
+        break;
+    case 85:
+        patente = "Marechal";
+        break;  
+
+    default:
+        patente = "Recruta";
+        break;
+}
+    
+let inventario = [ "Kit de Primeiros Socorros", "Armadura de Kevlar", "Lanterna Tática", "Granada de Fumaça" ];  // Inventário do jogador
+let quantidade = 0;
+
+for (let i = 0; i < inventario.length; i++) {  // Laço de repetição para contar a quantidade de itens no inventário
+    quantidade += 1;
+}
+
+console.log("O Herói " + jogador + " está no nível " + nivel + " com o saldo de " + saldoVitorias + " vitórias e está no nível " + ranking + " com a patente de " + patente + ".");
+
+console.log(jogador +" possui " + quantidade + " itens no inventário: " + inventario.join(", "));
+
+
+
+
